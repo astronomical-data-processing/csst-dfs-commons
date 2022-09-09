5738540b5780c05474070b919b88f01ad5a43bf7
shoulinwei <weishoulin@astrolab.cn>
2022-09-10 00:01:33 +0800

    cpic and bug

--- csst_dfs_commons/utils/fits.py
+++ csst_dfs_commons/utils/fits.py
@@ -1,5 +1,8 @@
 def get_header_value(key: str, header, default_value = None):
     try:
-        return header[key]
+        v = header[key]
+        if type(v) == str:
+            return v.strip()
+        return v
     except Exception as e:
         return default_value
