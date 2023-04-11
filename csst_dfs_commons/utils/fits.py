95ae816a76348bf11fbc414732ebbe2b32429a97
shoulinwei <weishoulin@astrolab.cn>
2023-04-11 22:28:06 +0800

    except catch

--- csst_dfs_commons/utils/fits.py
+++ csst_dfs_commons/utils/fits.py
@@ -21,9 +21,12 @@ def get_header_value(key: str, headers, default_value = None):
         return default_value
 
 def get_healpix_id(ra, dec, nside=32):
-    hp = HEALPix(nside=nside, order='nested', frame=ICRS())
-    coord = SkyCoord(ra = ra * u.deg, dec = dec * u.deg, frame='icrs')
-    return hp.skycoord_to_healpix(coord)
+    try:
+        hp = HEALPix(nside=nside, order='nested', frame=ICRS())
+        coord = SkyCoord(ra = ra * u.deg, dec = dec * u.deg, frame='icrs')
+        return hp.skycoord_to_healpix(coord)
+    except:
+        return -1
 
 def get_healpix_ids(ra, dec, radius, nside=32):
     hp = HEALPix(nside=nside, order='nested', frame=ICRS())
